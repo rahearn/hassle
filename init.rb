b58517976acad3f2f30214fba6165f8a8e90e540
Trym <ts@skalar.no>
2010-03-09 01:35:37 +0100

    Rails 3 compatibility

--- init.rb
+++ init.rb
@@ -1,3 +1,7 @@
-if RAILS_ENV == 'production'
-	ActionController::Dispatcher.middleware.use Hassle
+if (defined? Rails.env ? Rails.env : RAILS_ENV) == "production"
+  if defined? Rails.configuration && defined? Rails.configuration.middleware
+    Rails.configuration.middleware.use(Hassle)
+  elsif defined? ActionController::Dispatcher && defined? ActionController::Dispatcher.middleware
+    ActionController::Dispatcher.middleware.use Hassle
+  end  
 end
