e5374c3acd7ad6eb79b289954da058519b673690
Jacques Crocker <railsjedi@gmail.com>
2010-03-10 15:06:06 -0800

    Fixing the sass template_location generation code so it correctly supports hashes and nonstandard locations. Adds support for compass

--- lib/hassle.rb
+++ lib/hassle.rb
@@ -25,7 +25,7 @@ class Hassle::Compiler
     expanded = File.expand_path(path)
     public_dir = File.join(File.expand_path(Dir.pwd), "public")
 
-    File.expand_path(compile_location(expanded.gsub(public_dir, ''), '..'))
+    File.expand_path(compile_location(expanded.gsub(public_dir, '')))
   end
 
   def compile_location(*subdirs)
@@ -37,11 +37,11 @@ class Hassle::Compiler
 
     if template_location.is_a?(Hash) || template_location.is_a?(Array)
       options[:template_location] = template_location.to_a.map do |input, output|
-        [input, css_location(input)]
+        [input, css_location(output)]
       end
     else
       default_location = File.join(options[:css_location], "sass")
-      options[:template_location] = {default_location => css_location(default_location)}
+      options[:template_location] = {default_location => File.expand_path(css_location(default_location), '..'))}
     end
   end
 
