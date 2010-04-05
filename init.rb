58d670a7319c7067dc99fe16b5b3ee1e28fa2e71
Jakob Skjerning <jakob@mentalized.net>
2010-04-05 22:11:51 +0200

    Proper check for production environment

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-if (defined? Rails.env ? Rails.env : RAILS_ENV) == "production"
+if (defined? Rails.env ? Rails.env.production? : "production" == RAILS_ENV)
   if defined? Rails.configuration && defined? Rails.configuration.middleware
     Rails.configuration.middleware.use(Hassle)
   elsif defined? ActionController::Dispatcher && defined? ActionController::Dispatcher.middleware
