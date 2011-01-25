c3e5f3df835831ad6bbe8fa739783eb237d00a05
Ryan Ahearn <ryan@craftsoftwaresolutions.com>
2011-01-25 10:20:55 -0500

    ensure parameters are passed to correct method

--- lib/hassle/railtie.rb
+++ lib/hassle/railtie.rb
@@ -7,7 +7,7 @@ module HassleRails
       require 'sass'
       require 'sass/plugin'
 
-      if (defined? Rails.env ? Rails.env.production? : "production" == RAILS_ENV)
+      if (defined?(Rails.env) ? Rails.env.production? : "production" == RAILS_ENV)
         if defined? Rails.configuration && defined? Rails.configuration.middleware
           Rails.configuration.middleware.use(Hassle)
         elsif defined? ActionController::Dispatcher && defined? ActionController::Dispatcher.middleware
