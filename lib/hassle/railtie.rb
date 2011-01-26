386743568f562d3e602b5db96f3f90919c6bb771
Ryan Ahearn <ryan@craftsoftwaresolutions.com>
2011-01-26 17:11:23 -0500

    Ensure all defined? calls use correct parameters

--- lib/hassle/railtie.rb
+++ lib/hassle/railtie.rb
@@ -8,9 +8,9 @@ module HassleRails
       require 'sass/plugin'
 
       if (defined?(Rails.env) ? Rails.env.production? : "production" == RAILS_ENV)
-        if defined? Rails.configuration && defined? Rails.configuration.middleware
-          Rails.configuration.middleware.use(Hassle)
-        elsif defined? ActionController::Dispatcher && defined? ActionController::Dispatcher.middleware
+        if defined?(Rails.configuration) && defined?(Rails.configuration.middleware)
+          Rails.configuration.middleware.use Hassle
+        elsif defined?(ActionController::Dispatcher) && defined?(ActionController::Dispatcher.middleware)
           ActionController::Dispatcher.middleware.use Hassle
         end  
       end
